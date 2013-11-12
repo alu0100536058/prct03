2093016e688b3d919686fe8ee68b880633f6001c
OpenShift guest <alu0100003369@ull.edu.es>
2013-11-12 18:18:08 -0500

    "Ampliado el numero de funciones que se representan hasta 14"

--- src/main2.py
+++ src/main2.py
@@ -1,12 +1,15 @@
 import modulo_equal  
 
-import math as mt
+#import math as mt
+from math import *
 
-exp = mt.exp
-log = mt.log
-sin = mt.sin
-cos = mt.cos
-tan = mt.tan
+
+#exp = mt.exp
+#log = mt.log
+#sin = mt.sin
+#cos = mt.cos
+#tan = mt.tan
+#asinh = mt.asinh
 
 # Valores por defecto
 
@@ -17,20 +20,25 @@ numero_test = 200
 expr1 = 'a+b'
 expr2 = 'b+a'
 
-expresiones = [ ('a**3*b**3','(a*b)**3') ,
+expresiones = [ ('(a*b)**3','(a**3)*(b**3)') ,
 ('a/b','1/(b/a)') ,
-('exp(a+b)','exp(a)*exp(b)'),
-('a-b','-(b-a)') ,  ('a**4*b**4','(a*b)**4') , 
-('(a+b)**2','a**2+2*a*b+b**2') , 
-('(a+b)*(a-b)','a**2-b**2') , 
+('exp(a+b)','(exp(a))*(exp(b))'),
+('log(a)**b','b*log(a)') ,
+('a-b','-(b-a)') ,  
+('(a*b)**4','(a**4)*(b**4)') , 
+('(a+b)**2','(a**2)+(2*a*b)+(b**2)') , 
+('(a+b)*(a-b)','(a**2)-(b**2)') , 
 ('log(a*b)','log(a)+log(b)') , 
 ('a*b','exp(log(a)+log(b))') ,
 ('1/((1/a)+(1/b))' , '(a*b)/(a+b)'),
-('tan(a+b)' , 'sin(a+b)/cos(a+b)') ,
-('sin(a+b)' , 'sin(a)*cos(b)+sin(b)*cos(a)')]
+('(a*(sin(b))**2)+((cos(b))**2)' , 'a') ,
+#('sinh(a+b)' , '(((exp(a)*exp(b))-(exp(-a)*exp(-b)))/2)') ,
+('tan(a+b)' , '(sin(a+b))/(cos(a+b))') ,
+('sin(a+b)' , 'sin(a)*cos(b)+sin(b)*cos(a)')
+]
 
 #('a*(sin**2(a)+cos**2(b))' , 'a')
-# 
+ 
 
 
 print "Tabla de prueba de expresiones: "
@@ -49,4 +57,3 @@ for i in expresiones:      #recorre las tuplas
 #	expr1 = i[0] #asigna a expr1 el primer elemento de cada tupla
 #	expr2 = i[1] #asigna a expr2 el segundo elemento de cada tupla
 #	print "%s %20s %20.1f %10.1f %10d %10d" % (expr1, expr2, min_value, max_value, numero_test, modulo_equal.equal( expr1 , expr2 , min_value , max_value , numero_test ))  
-	
